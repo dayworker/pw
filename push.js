0088eb567f322458becdc2de64c2220ed77c969d
YoungJin Kim <dayworker@wrapcore.co.kr>
2023-06-09 10:02:31 +0900

    -- 수정

--- push.js
+++ push.js
@@ -12,6 +12,15 @@ push.setVapidDetails(
 	vapidKeys.privateKey
 );
 
-let sub = {};
+let sub = {
+	endpoint:
+		"https://wns2-pn1p.notify.windows.com/w/?token=BQYAAAB8VnEuAntWv7P68IoWeKDkSSovu0Bh%2fsot0Bji%2bHXhs3%2bLXFLw1%2bDc7tVW%2bU2mdORgseIDicPvYd7uns2fodLYYKv7dPz6PvTZRNOPZMBpUWO7uHEWV9QpZld49YTEux7AjZf7uNe%2bMPw8u6ZivcnjdMr4WRy7fo1TjOUvr0ZDVYSYOLYl6s4MDtokXUxYfA5pC%2ft9W8FSG93SoVAlAqeCRt%2fgQP0OyB%2bbassq493O7oM9Qrg21dgrs5bmBmmzpLQQQITtQYmobkP%2bWSXog6Ixw7wR%2fN2bzniNKGXbP8rj17vDvoV0QsLUnMyIuq9ZUc5bj1rna7OTcs7Z%2feCq12dQ",
+	expirationTime: null,
+	keys: {
+		p256dh:
+			"BM7P6tDi9KPD7YwtMbZBiRtghB58tQ-s6fsyTtBPvOUmMuWDr4GfOyB32FugRrt4qbPGDgPUM-5_qICl5dULRKY",
+		auth: "EzCwhXREwkPItiFDX9f5yQ",
+	},
+};
 
 push.sendNotification(sub, "test message");
