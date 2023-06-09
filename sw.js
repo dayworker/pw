d35fa54afee795e5bcbfa686f2d2ff6a3901beee
YoungJin Kim <dayworker@wrapcore.co.kr>
2023-06-09 09:48:56 +0900

    -- 수정2

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-// self.addEventListener("push", () => {
-//   self.registration.showNotification("Hello world!", {});
-// });
-self.addEventListener("push", (event) => {
-	const data = e.data.json();
-	let title = data.title || "a default message if nothing was passed to us";
-	let body = data.body || "WOW! The things I learned at FloridaJS";
-	let tag = data.tag || "push-simple-demo-notification-tag";
-	let icon = "https://floridajs.com/images/logo.jpg";
-
-	event.waitUntil(self.registration.showNotification(title, { body, tag }));
+self.addEventListener("push", () => {
+	self.registration.showNotification("Hello world!", {});
 });
+// self.addEventListener("push", (event) => {
+// 	const data = e.data.json();
+// 	let title = data.title || "a default message if nothing was passed to us";
+// 	let body = data.body || "WOW! The things I learned at FloridaJS";
+// 	let tag = data.tag || "push-simple-demo-notification-tag";
+// 	let icon = "https://floridajs.com/images/logo.jpg";
+
+// 	event.waitUntil(self.registration.showNotification(title, { body, tag }));
+// });
